05547272a3e391910b9eb3e5d19fdfccd0df8f5b
Covfefeable <13128568934@163.com>
2022-04-03 16:45:27 +0800

    add kdj api

--- analysis/urls.py
+++ analysis/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('basic/', I_basic.basic, name='basic'),
     path('search/', I_basic.search, name='search'),
     path('macd/', views.macd, name='macd'),
+    path('kdj/', views.kdj, name='kdj'),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -138,3 +138,83 @@ def macd(request):
     df3.to_json(file, orient='split')
 
     return HttpResponse(res)
+
+def kdj(request):
+    stock_code = request.GET.get("stock_code")
+    start_date = request.GET.get("start_date")
+    end_date = request.GET.get("end_date")
+    data = "date,code,high,close,low,tradeStatus"
+
+    if not stock_code:
+        jsonres = {
+            'code': '500',
+            'msg': '股票代码(stock_code)不能为空'
+        }
+        return HttpResponse(json.dumps(jsonres))
+    if not start_date:
+        jsonres = {
+            'code': '500',
+            'msg': '开始日期(start_date)不能为空'
+        }
+        return HttpResponse(json.dumps(jsonres))
+    if not end_date:
+        jsonres = {
+            'code': '500',
+            'msg': '起始时间(end_date)不能为空'
+        }
+        return HttpResponse(json.dumps(jsonres))
+
+    file = "./cache/KDJ_" + stock_code + "_" + start_date + "_" + end_date + ".json"
+    if os.path.exists(file):
+        with open(file,'r') as load_f:
+            load_dict = json.load(load_f)
+            return HttpResponse(json.dumps(load_dict))
+
+    rs = bs.query_history_k_data_plus(
+        stock_code,
+        data,
+        start_date=start_date,
+        end_date=end_date,
+        frequency="d",
+        adjustflag="3")
+
+    if rs.error_msg != 'success':
+        jsonres = {
+            'code': '500',
+            'msg': rs.error_msg
+        }
+        return HttpResponse(json.dumps(jsonres))
+
+    data_list = []
+    while (rs.error_code == '0') & rs.next():
+        data_list.append(rs.get_row_data())
+    result = pd.DataFrame(data_list, columns=rs.fields)
+    df_status = result[result['tradeStatus'] == '1']
+
+    low = df_status['low'].astype(float)
+    del df_status['low']
+    df_status.insert(0, 'low', low)
+    high = df_status['high'].astype(float)
+    del df_status['high']
+    df_status.insert(0, 'high', high)
+    close = df_status['close'].astype(float)
+    del df_status['close']
+    df_status.insert(0, 'close', close)
+
+    # 计算KDJ指标,前9个数据为空
+    low_list = df_status['low'].rolling(window=9).min()
+    high_list = df_status['high'].rolling(window=9).max()
+    rsv = (df_status['close'] - low_list) / (high_list - low_list) * 100
+    df_data = pd.DataFrame()
+    df_data['K'] = rsv.ewm(com=2).mean()
+    df_data['D'] = df_data['K'].ewm(com=2).mean()
+    df_data['J'] = 3 * df_data['K'] - 2 * df_data['D']
+    df_data.index = df_status['date'].values
+    df_data.index.name = 'date'
+    # 删除空数据
+    df_data = df_data.dropna()
+
+    res = df_data.to_json( orient='split')
+    df_data.to_json(file, orient='split')
+
+    return HttpResponse(res)
